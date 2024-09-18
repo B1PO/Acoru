a3f3bc85f5fcae10c423ab9f43df625e05344378
Pedro Prado <pedroprado1418@gmail.com>
2024-09-18 12:44:56 -0600

    Initial Commit

---
+++ Acoru/AcoruApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcoruApp.swift
+//  Acoru
+//
+//  Created by Pedro Prado on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AcoruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
