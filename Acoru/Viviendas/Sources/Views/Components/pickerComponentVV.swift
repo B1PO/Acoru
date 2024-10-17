1630a62d0cb742c2fc5a9b809ee5c1bf01d87c5b
Jose Alejandro Perez Chavez <jackinjaxx2002@gmail.com>
2024-10-17 14:49:35 -0600

    creacion picker

---
+++ Acoru/Viviendas/Sources/Views/Components/pickerComponentVV.swift
@@ -0,0 +1,18 @@
+//
+//  pickerComponentVV.swift
+//  Acoru
+//
+//  Created by Jose Alejandro Perez Chavez on 17/10/24.
+//
+
+import SwiftUI
+
+struct pickerComponentVV: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    pickerComponentVV()
+}
--- Acoru/Viviendas/Sources/pene2/claroquesi.swift
+++
@@ -1,7 +0,0 @@
-//
-//  claroquesi.swift
-//  Acoru
-//
-//  Created by Jose Alejandro Perez Chavez on 17/10/24.
-//
-
