db7516ad754179ae841ba54a217c087457471b00
Mario Moreno <mariormz246@gmail.com>
2024-10-18 14:11:17 -0600

    merge a main

--- Acoru/ZonasRiesgo/Sources/Views/Screens/zonasViewZR.swift
+++ Acoru/ZonasRiesgo/Sources/Views/Screens/zonasViewZR.swift
@@ -2,6 +2,17 @@
 //  zonasViewZR.swift
 //  Acoru
 //
-//  Created by Mauricio Betancourt Mora on 14/10/24.
+//  Created by Mario Moreno on 10/17/24.
 //
 
+import SwiftUI
+
+struct zonasViewZR: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    zonasViewZR()
+}
